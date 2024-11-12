48ba1e3ea28683b9087cc04f6ea33545210b53ae
akuahamda <hamdalamohammed80@gmail.com>
2024-11-11 17:56:53 -0800

    added forecast API

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// import axios from "axios";
  const apiKey = "8fob310357070bce9t79c94a1a8a4f49";
 
 
@@ -10,12 +9,13 @@ let windSpeed = document.querySelector("#wind");
 let cityName = document.querySelector(".city-name");
 
 
-function weatherSearch(event) {
-    event.preventDefault();
-    let cityInput = document.querySelector(".form").value;
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}`;
 
-    axios.get(apiUrl).then((result)=>{
+function weatherSearch(event) {
+  let cityInput = document.querySelector(".form").value;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}`;
+    let forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${cityInput}&key=${apiKey}`
+
+     axios.get(apiUrl).then((result)=>{
         iconImg.src = result?.data.condition?.icon_url;
         temperature.innerHTML = Math.round(result.data.temperature.current);
         description.innerHTML = result.data.condition.description;
@@ -25,35 +25,60 @@ function weatherSearch(event) {
 
     })
 
+
+    axios.get(forecastUrl).then((result)=> {
+      displayForecast(result)
+    }
+  )
+
 }
-let input = document.querySelector("form");
-input.addEventListener("submit", weatherSearch);
+
+window.addEventListener('load',weatherSearch)
+let formTag = document.querySelector("form")
+formTag.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  weatherSearch()
+
+});
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
 
 
-// {
-//     "city": "Paris",
-//     "country": "France",
-//     "coordinates": {
-//         "longitude": 2.3200410217200766,
-//         "latitude": 48.8588897
-//     },
-//     "condition": {
-//         "description": "overcast clouds",
-//         "icon_url": "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png",
-//         "icon": "broken-clouds-night"
-//     },
-//     "temperature": {
-//         "current": 10.15,
-//         "humidity": 96,
-//         "feels_like": 9.73,
-//         "pressure": 1031
-//     },
-//     "wind": {
-//         "speed": 3.6,
-//         "degree": 30
-//     },
-//     "time": 1730926501
-// }
+
+
+function displayForecast(results) { 
+
+  let forecast = document.querySelector(".forecast");
+  forecast.innerHTML = ""
+
+
+  let days = results?.data?.daily;
+  days.slice(0,5)?.forEach (function (day) { 
+    forecast.innerHTML += `<div>
+          <div>${formatDay(day.time)}</div>
+          <div class="forecast-icon">
+          <img src=${day?.condition?.icon_url} />
+          </div>
+          <div class="forecast">
+            <div><strong>${Math.round(day?.temperature?.maximum)}&deg;</strong></div>
+            <div>${Math.round(day?.temperature?.minimum)}&deg;</div>
+          </div>
+        </div>
+
+        `;
+        
+      });
+
+
+  };
+ 
+
+
 
 
 
